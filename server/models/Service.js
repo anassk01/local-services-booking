a16a84ccf725d052efba1aa0ded20926d217622f
anassk01 <akabil9999@gmail.com>
2026-08-17 10:42:00 +0100

    Add service model

---
+++ server/models/Service.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const serviceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0.01,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Service", serviceSchema);
