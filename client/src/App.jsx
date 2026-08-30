16f63ab20e7e104e2bb8ac30cc94b27ec821b291
anassk01 <akabil9999@gmail.com>
2026-08-30 11:19:00 +0100

    Add navigation links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
