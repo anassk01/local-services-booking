93af14f85f9ed1bcf83c82f254293dc643950fcc
anassk01 <akabil9999@gmail.com>
2026-09-08 09:01:00 +0100

    Add reservation cancellation

--- client/src/pages/MyReservations.jsx
+++ client/src/pages/MyReservations.jsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
 import { getMyReservations } from "../services/getMyReservations";
-
+import { cancelReservation } from "../services/cancelReservation";
 function MyReservations() {
   const [reservations, setReservations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [pending, setPending] = useState(false);
+  const [canceledReservation, setCanceledReservation] = useState("");
+  const [cancelError, setCancelError] = useState(null);
+  async function myReservations() {
+    try {
+      const results = await getMyReservations();
+      setReservations(results);
+    } catch {
+      setError("cannot get reservations");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function deleteReservation(id) {
+    setCancelError(null);
+    setPending(true);
+    setCanceledReservation(id);
+    try {
+      const canceled = await cancelReservation(id);
+      const remainingReservations = reservations.filter(
+        (item) => item._id !== canceled._id,
+      );
+      setReservations(remainingReservations);
+    } catch {
+      setCancelError("unable to delete reservation");
+    } finally {
+      setPending(false);
+    }
+  }
 
   useEffect(() => {
-    async function myReservations() {
-      try {
-        const results = await getMyReservations();
-        setReservations(results);
-      } catch {
-        setError("cannot get reservations");
-      } finally {
-        setLoading(false);
-      }
-    }
-
     myReservations();
   }, []);
   return (
@@ -42,12 +61,19 @@ function MyReservations() {
                 <div>{item.service.city}</div>
                 <div>{item.service.price}</div>
               </div>
+              <button
+                disabled={pending && item._id === canceledReservation}
+                onClick={() => deleteReservation(item._id)}
+              >
+                cancel reservation
+              </button>
             </div>
           );
         })
       ) : (
         <div>no reservations found</div>
       )}
+      {cancelError && <div>{cancelError}</div>}
     </>
   );
 }
