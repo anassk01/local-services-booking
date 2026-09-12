9885ce8fb4506cdfb84a0626506b8a3356caf2c5
anassk01 <akabil9999@gmail.com>
2026-09-12 09:04:00 +0100

    Add categories dashboard link

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -5,6 +5,7 @@ export default function AdminDashboard() {
       <div> AdminDashboard </div>
       <div>Administration area</div>
       <Link to="/admin/services">services</Link>
+      <Link to="/admin/categories">categories</Link>
     </>
   );
 }
