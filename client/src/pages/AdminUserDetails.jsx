b89e89fb1e9aaa1d9aef4de05abdcf615de6b38e
anassk01 <akabil9999@gmail.com>
2026-09-18 15:27:00 +0100

    Build admin user details page

---
+++ client/src/pages/AdminUserDetails.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { getUserById } from "../services/getUserById";
+import { useParams } from "react-router-dom";
+function AdminUserDetails() {
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function GetUserById() {
+      try {
+        const result = await getUserById(id);
+        setUser(result);
+      } catch {
+        setError("cannot get user details");
+      } finally {
+        setLoading(false);
+      }
+    }
+    GetUserById();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div></div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : Object.values(user).length > 0 ? (
+        <div>
+          <div>{user.name}</div>
+          <div>{user.email}</div>
+          <div>{user.role}</div>
+        </div>
+      ) : (
+        <div>cannot retrieve user details </div>
+      )}
+    </>
+  );
+}
+
+export default AdminUserDetails;
